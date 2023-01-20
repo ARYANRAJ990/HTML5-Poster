651a69e65ca277fc9cfe6b5db6e0cedbcec256d9
Arsh Vimal <arsh11vimal@gmail.com>
2023-01-20 19:35:29 +0530

    Still brainstorming
    Most Notable Change:
    +Added typed.js
    
    Everything Else:
    +Added more text
    +Added a quote
    +Did some CSS formatting

---
+++ script.js
@@ -0,0 +1,6 @@
+var Typed = new Typed(".auto-type",{
+    strings: ["Cleanliness begins with me, and ends with we.^1500","Cleanliness is not a choice, it's a responsibility.^1500","Cleanliness is not just about the environment, it's about respect.^1500","Cleanliness is not a one-time task, it's a daily habit.^1500","Cleanliness is a fundamental right.^1500", "Cleanliness is not only the absence of dirt but also the presence of beauty.^1500"],
+    typeSpeed: 20,
+    backSpeed: 20,
+    loop: true,
+})
