a610a8dba2cc96d365d0fcf0421e5dde5f0c9163
Arsh Vimal <arsh11vimal@gmail.com>
2023-01-22 21:38:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,33 +5,59 @@ var Typed = new Typed(".auto-type",{
     loop: true,
 })
 
-var frameIncrease = 0;
-frameCount= 1;
-var angle = 0;
-function update(){
-    angle+=1;
-    frameIncrease++;
-    if(frameIncrease == 24){
-        frameIncrease = 0;
-        frameCount++;
-        if (frameCount == 5){
-            frameCount = 1;
-        }
-    }
-
-}
-function frame(){
-    trashcan2.style.transform = "rotate(" + angle + "deg)";
-}
-function loop(){
-    update();
-    frame();
-    requestAnimationFrame(loop);
-}
 
 var trashcan1 = document.getElementById("trashcan1");
 var trashcan2 = document.getElementById("trashcan2");
 var Garbage1 = document.getElementById("Garbage1");
+
+function updateValues(t1l, t1t, t1r, t1o, t2l, t2t, t2r, t2o, g1l, g1t, g1r, g1o){
+    //trashcanlid
+    trashcan1.style.left = t1l;
+    trashcan1.style.top =  t1t;
+    trashcan1.style.transform = "rotate(" + t1r + "deg)";
+    trashcan1.style.transformOrigin = "\""+t1o+"\"";
+
+    //trashcanbody
+    trashcan2.style.left = t2l;
+    trashcan2.style.top =  t2t;
+    trashcan2.style.transform = "rotate(" + t2r + "deg)";
+    trashcan2.style.transformOrigin = t2o;
+
+    //garbage
+    Garbage1.style.left = g1l;
+    Garbage1.style.top =  g1t;
+    Garbage1.style.transform = "rotate(" + g1r + "deg)";
+    Garbage1.style.transformOrigin = g1o;
+}
+
+var frameIncrease = 0;
+frameCount= 0;
+function update(){
+    frameIncrease++;
+    pvalue = 0;
+    if (frameCount <= 5){
+        pvalue = 0;
+    }
+    else{
+        pvalue = frameCount;
+    }
+    if(frameIncrease == 8){
+        frameIncrease = 0;
+        frameCount++;
+        if (frameCount == 15){
+            frameCount = 15;
+        }
+    }
+    if (frameCount >= 0 && frameCount <= 15){
+        updateValues(0, 150, frameCount*4, "85%,50%", 0, 150, 0, "80% 100%", (-200+(frameCount*4)), (0+(frameCount*4)), -45, "50% 50%");
+    }
+}
+function loop(){
+    update();
+    requestAnimationFrame(loop);
+}
+
+loop();
 
 trashcan1.style.transformOrigin = "85% 50%";
 trashcan1.style.transform = "rotate(" + 0 + "deg)";
