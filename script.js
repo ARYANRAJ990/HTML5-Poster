90f04e321e81db0312b60f2e17c260196f36be37
Arsh Vimal <arsh11vimal@gmail.com>
2023-01-23 21:38:53 +0530

    Garbage Animation
    Finished Garbage animation

--- script.js
+++ script.js
@@ -35,21 +35,88 @@ frameCount= 0;
 function update(){
     frameIncrease++;
     pvalue = 0;
-    if (frameCount <= 5){
+    if (frameCount <= 4){
         pvalue = 0;
     }
     else{
-        pvalue = frameCount;
+        pvalue = frameCount-5;
     }
-    if(frameIncrease == 8){
+    if(frameIncrease == 4){
         frameIncrease = 0;
         frameCount++;
-        if (frameCount == 15){
-            frameCount = 15;
+        if (frameCount >= 100){
+            frameCount = 0;
         }
+    console.log(frameCount);
     }
     if (frameCount >= 0 && frameCount <= 15){
-        updateValues(0, 150, frameCount*4, "85%,50%", 0, 150, 0, "80% 100%", (-200+(frameCount*4)), (0+(frameCount*4)), -45, "50% 50%");
+        updateValues(0, 150, frameCount*4, "85%,50%", 0, 150, 0, "80% 100%", (-200+(pvalue*20)), (0+(pvalue*20)), -45, "50% 50%");
+    }
+    if (frameCount == 16){
+        updateValues(0, 150, 60, "85%,50%", 0, 150, 0, "80% 100%", 20, 225, -35, "50% 50%");
+    }
+    if (frameCount == 17){
+        updateValues(5, 150, 59, "85%,50%", 0, 150, 1, "80% 100%", 25, 225, -35, "50% 50%");
+    }
+    if (frameCount == 18){
+        updateValues(10, 150, 58, "85%,50%", 0, 150, 2, "80% 100%", 26, 225, -35, "50% 50%");
+    }
+    if (frameCount == 19){
+        updateValues(12, 150, 60, "85%,50%", 0, 150, 4, "80% 100%", 28, 225, -35, "50% 50%");
+    }
+    if (frameCount == 20){
+        updateValues(14, 150, 61, "85%,50%", 0, 150, 4, "80% 100%", 30, 225, -34, "50% 50%");
+    }
+    if (frameCount == 21){
+        updateValues(14, 150, 62, "85%,50%", 0, 150, 6, "80% 100%", 35, 226, -31, "50% 50%");
+    }
+    if (frameCount == 22){
+        updateValues(16, 150, 59, "85%,50%", 0, 150, 6, "80% 100%", 36, 228, -31, "50% 50%");
+    }
+    if (frameCount == 23){
+        updateValues(16, 150, 60, "85%,50%", 0, 150, 6, "80% 100%", 36, 230, -30, "50% 50%");
+    }
+    if (frameCount == 24){
+        updateValues(16, 150, 60, "85%,50%", 0, 150, 6, "80% 100%", 36, 234, -27, "50% 50%");
+    }
+    if (frameCount == 25){
+        updateValues(14, 150, 60, "85%,50%", 0, 150, 7, "80% 100%", 36, 240, -23, "50% 50%");
+    }
+    if (frameCount == 26){
+        updateValues(14, 150, 55, "85%,50%", 0, 150, 6, "80% 100%", 36, 239, -20, "50% 50%");
+    }
+    if (frameCount == 27){
+        updateValues(14, 150, 50, "85%,50%", 0, 150, 6, "80% 100%", 37, 238, -17, "50% 50%");
+    }
+    if (frameCount == 28){
+        updateValues(12, 150, 45, "85%,50%", 0, 150, 4, "80% 100%", 37, 245, -14, "50% 50%");
+    }
+    if (frameCount == 29){
+        updateValues(11, 150, 40, "85%,50%", 0, 150, 3, "80% 100%", 38, 250, -11, "50% 50%");
+    }
+    if (frameCount == 30){
+        updateValues(10, 150, 35, "85%,50%", 0, 150, 2, "80% 100%", 39, 255, -8, "50% 50%");
+    }
+    if (frameCount == 31){
+        updateValues(8, 150, 30, "85%,50%", 0, 150, 1, "80% 100%", 40, 260, -5, "50% 50%");
+    }
+    if (frameCount == 32){
+        updateValues(6, 150, 25, "85%,50%", 0, 150, 0, "80% 100%", 40, 266, -2, "50% 50%");
+    }
+    if (frameCount == 33){
+        updateValues(4, 150, 20, "85%,50%", 0, 150, 0, "80% 100%", 40, 272, 0, "50% 50%");
+    }
+    if (frameCount == 34){
+        updateValues(2, 150, 15, "85%,50%", 0, 150, 0, "80% 100%", 40, 279, 0, "50% 50%");
+    }
+    if (frameCount == 35){
+        updateValues(0, 150, 10, "85%,50%", 0, 150, 0, "80% 100%", 40, 287, 0, "50% 50%");
+    }
+    if (frameCount == 36){
+        updateValues(0, 150, 5, "85%,50%", 0, 150, 0, "80% 100%", 40, 290, 0, "50% 50%");
+    }
+    if (frameCount == 37){
+        updateValues(0, 150, 0, "85%,50%", 0, 150, 0, "80% 100%", 40, 290, 0, "50% 50%");
     }
 }
 function loop(){
@@ -58,12 +125,3 @@ function loop(){
 }
 
 loop();
-
-trashcan1.style.transformOrigin = "85% 50%";
-trashcan1.style.transform = "rotate(" + 0 + "deg)";
-trashcan2.style.transformOrigin = "80% 100%";
-trashcan2.style.transform = "rotate(" + 0 + "deg)";
-Garbage1.style.transform = "rotate(" + -45 + "deg)";
-Garbage1.style.left = "-200";
-Garbage1.style.top = "0";
-Garbage1.style.transformOrigin = "50% 50%";
